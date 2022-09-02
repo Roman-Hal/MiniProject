586c85d4b8bdd0508e45ff9f782fe248bcd74630
Aaron-CYF <86688603+Aaron-CYF@users.noreply.github.com>
2022-09-02 15:38:25 +0100

    make footer work on different pages

--- src/Footer.js
+++ src/Footer.js
@@ -1,17 +1,50 @@
 import React from 'react';
-// import { HashRouter, Routes, Route } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import "./App.css";
 
 const Footer = () => {
+  const path = useLocation().pathname;
+
+  const data = {
+    home: { text: "This is the home page" },
+    eval: { text: "e" },
+    avail: { text: "a" },
+    futr: { text: "f" },
+    abtus: { text: "t" }
+  };
+
+  let footerContent = "";
+
+  switch (path) {
+    case "/":
+      footerContent = data.home.text;
+      break;
+    case "/evaluation":
+      footerContent = data.eval.text;
+      break;
+    case "/availability":
+      footerContent = data.avail.text;
+      break;
+    case "/future":
+      footerContent = data.futr.text;
+      break;
+    case "/aboutus":
+      footerContent = data.abtus.text;
+      break;
+    default:
+      footerContent = "";
+  }
+
   return (
     <footer>
       {/* <Routes>
-        <Route exact path="/" element={"This is the home page "} />
-        <Route exact path="/evaluation" element={"<Evaluation />"} />
-        <Route exact path="/availability" element={"<Availability />"} />
-        <Route exact path="/future" element={"<Future />"} />
-        <Route exact path="/aboutus" element={"<Aboutus />"} />
+        <Route match="/" element={"This is the home page "} />
+        <Route match="/evaluation" element={"<Evaluation />"} />
+        <Route match="/availability" element={"<Availability />"} />
+        <Route match="/future" element={"<Future />"} />
+        <Route match="/aboutus" element={"<Aboutus />"} />
       </Routes> */}
+      {footerContent}
       Hi everyone!!
     </footer>
   );
