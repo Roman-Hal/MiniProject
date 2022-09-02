1182924dd6d02aba07cdf63c20af5358e60bbc06
Aaron-CYF <86688603+Aaron-CYF@users.noreply.github.com>
2022-09-02 17:52:56 +0100

    about us update

--- src/Aboutus.js
+++ src/Aboutus.js
@@ -3,15 +3,24 @@ import React from 'react'
 const Aboutus = () => {
   return (
     <div>
+      <h2>About Us</h2>
+      <section className="content">
         <h4>Created By: </h4>
-        <section className='aboutUs'>
-        Name: Aaron<br></br>
-        Github: aaron-cyf<br></br><br></br>
-        Name: Roman<br></br>
-        Github: Roman-Hal<br></br><br></br>
-        Name: Sobia<br></br>
-        Github:sobiawaqar<br></br>
-        </section>
+        <span className='about-us-card'>
+          Name: Aaron<br></br>
+          Github: <a target="_blank" rel="noreferrer" href='https://github.com/aaron-cyf'>Aaron-CYF</a><br></br>
+          <br></br>
+        </span>
+        <span className='about-us-card'>
+          Name: Roman<br></br>
+          Github: <a target="_blank" rel="noreferrer" href='https://github.com/roman-hal'>Roman-Hal</a><br></br>
+          <br></br>
+        </span>
+        <span className='about-us-card'>
+          Name: Sobia<br></br>
+          Github: <a target="_blank" rel="noreferrer" href='https://github.com/sobiawaqar'>sobiawaqar</a><br></br>
+        </span>
+      </section>
     </div>
   );
 }
